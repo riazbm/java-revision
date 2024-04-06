6a33805837e4a24a48572d7213cb8fc7b31e9492
riziki <rizikimohamed968@gmail.com>
2024-04-06 12:35:26 +0300

    use of console input, the scanner

--- src/main/java/org/example/chapter1/Example1.java
+++ src/main/java/org/example/chapter1/Example1.java
@@ -1,14 +1,16 @@
 package org.example.chapter1;
-
+import java.util.Scanner;
+import java.util.Scanner;
 public class Example1 {
     public static void main(String[] args){
-        double radius;
-        double area;
+        Scanner input = new Scanner(System.in);
 
-        radius = 21;
+        System.out.println("enter the number for radius: ");
+        double radius = input.nextDouble();
+        double area = input.nextDouble();
 
         area = radius * radius * 3.14;
 
-        System.out.println("area of a circle is" +area);
+        System.out.println("area of a circle is" + area);
     }
 }
