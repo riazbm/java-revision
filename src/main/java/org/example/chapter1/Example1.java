980f7760aaf298ffa26b838a8f3420f49b5598a9
riziki <rizikimohamed968@gmail.com>
2024-04-06 12:53:41 +0300

    use of final keyword to declare contant words

--- src/main/java/org/example/chapter1/Example1.java
+++ src/main/java/org/example/chapter1/Example1.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 public class Example1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        final double PI = 3.141;
+        double area;
+        double circumference;
 
         System.out.println("enter the number for radius: ");
         double radius = input.nextDouble();
-        double area = input.nextDouble();
 
-        area = radius * radius * 3.14;
+        area = radius * radius * PI;
+        circumference = PI * (radius * 2);
 
         System.out.println("area of a circle is" + area);
+        System.out.println("circumference of the circle is " + circumference);
     }
 }
