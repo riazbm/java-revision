5dce483bad1db938560217fcc3a576cff3bc7738
riziki <rizikimohamed968@gmail.com>
2024-04-06 15:32:20 +0300

    determining leap year

--- src/main/java/org/example/chapter1/example2.java
+++ src/main/java/org/example/chapter1/example2.java
@@ -40,5 +40,11 @@ public class example2 {
         else {
             System.out.println("Obese");
         }
+//        boolean operators
+        System.out.println("Enter a year:");
+        int year = input.nextInt();
+
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        System.out.println(year + "is a leap year? " + isLeapYear);
     }
 }
