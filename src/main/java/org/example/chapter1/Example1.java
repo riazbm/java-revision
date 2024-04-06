17abbadbab8698813eacd0068b491ff5af5d4e6c
riziki <rizikimohamed968@gmail.com>
2024-04-06 12:14:24 +0300

    example1 done

---
+++ src/main/java/org/example/chapter1/Example1.java
@@ -0,0 +1,14 @@
+package org.example.chapter1;
+
+public class Example1 {
+    public static void main(String[] args){
+        double radius;
+        double area;
+
+        radius = 21;
+
+        area = radius * radius * 3.14;
+
+        System.out.println("area of a circle is" +area);
+    }
+}
